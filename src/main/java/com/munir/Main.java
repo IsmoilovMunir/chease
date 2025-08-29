1bd3b8ffc736046f26981047a108e5d738f2f784
MunirIsmoilov <ismoilov.munir@gmail.com>
2025-08-29 18:49:34 +0300

    created a console board and inserted all the figures with Unicode there

--- src/main/java/com/munir/Main.java
+++ src/main/java/com/munir/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         board.setupDefaultPiecesPositions();
+        BoardConsoleRenderer renderer = new BoardConsoleRenderer();
+        renderer.render(board);
         int a =123;
     }
 }
